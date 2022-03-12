c9d86d34e9d79a856d1c709e2bf6fef1132a47d9
$GITHUB_ACTOR <>
2022-03-12 15:37:23 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,4 +1,4 @@
-m3o contact update --phones='[
+m3o contact update --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -12,12 +12,7 @@ m3o contact update --phones='[
     "label": "blog",
     "url": "https://blog.joe.me"
   }
-]' --addresses='[
-  {
-    "label": "company address",
-    "location": "123 street address"
-  }
-]' --note="this person is very important" --social_medias='[
+]' --social_medias='[
   {
     "label": "twitter",
     "username": "joe-twitter"
@@ -26,7 +21,7 @@ m3o contact update --phones='[
     "label": "facebook",
     "username": "joe-facebook"
   }
-]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --emails='[
+]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --emails='[
   {
     "address": "home@example.com",
     "label": "home"
@@ -35,4 +30,9 @@ m3o contact update --phones='[
     "address": "work@example.com",
     "label": "work"
   }
-]' --birthday="1995-01-01" 
+]' --birthday="1995-01-01" --addresses='[
+  {
+    "label": "company address",
+    "location": "123 street address"
+  }
+]' --note="this person is very important" 
