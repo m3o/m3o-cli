c880dcd1f0be3ecdc74d1f4babb8e345362a13c1
$GITHUB_ACTOR <>
2022-03-15 11:20:26 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,4 +1,4 @@
-m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
+m3o contact update --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -6,20 +6,6 @@ m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --ph
   {
     "label": "work",
     "number": "010-87654321"
-  }
-]' --birthday="1995-01-01" --emails='[
-  {
-    "address": "home@example.com",
-    "label": "home"
-  },
-  {
-    "address": "work@example.com",
-    "label": "work"
-  }
-]' --links='[
-  {
-    "label": "blog",
-    "url": "https://blog.joe.me"
   }
 ]' --addresses='[
   {
@@ -35,4 +21,18 @@ m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --ph
     "label": "facebook",
     "username": "joe-facebook"
   }
-]' --note="this person is very important" 
+]' --note="this person is very important" --id="42e48a3c-6221-11ec-96d2-acde48001122" --emails='[
+  {
+    "address": "home@example.com",
+    "label": "home"
+  },
+  {
+    "address": "work@example.com",
+    "label": "work"
+  }
+]' --links='[
+  {
+    "label": "blog",
+    "url": "https://blog.joe.me"
+  }
+]' --birthday="1995-01-01" 
