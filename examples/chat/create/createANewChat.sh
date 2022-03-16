4c1c3b63ebf251abc48d3527df6549e1ca6ccdb7
$GITHUB_ACTOR <>
2022-03-16 15:52:46 +0000

    Commit from m3o/m3o-cli action

--- examples/chat/create/createANewChat.sh
+++ examples/chat/create/createANewChat.sh
@@ -1,1 +1,1 @@
-m3o chat create --name="general" --description="The general chat room" 
+m3o chat create --description="The general chat room" --name="general" 
--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,32 +1,4 @@
-m3o contact update --name="joe" --emails='[
-  {
-    "address": "home@example.com",
-    "label": "home"
-  },
-  {
-    "address": "work@example.com",
-    "label": "work"
-  }
-]' --addresses='[
-  {
-    "label": "company address",
-    "location": "123 street address"
-  }
-]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
-  {
-    "label": "home",
-    "number": "010-12345678"
-  },
-  {
-    "label": "work",
-    "number": "010-87654321"
-  }
-]' --links='[
-  {
-    "label": "blog",
-    "url": "https://blog.joe.me"
-  }
-]' --birthday="1995-01-01" --social_medias='[
+m3o contact update --name="joe" --social_medias='[
   {
     "label": "twitter",
     "username": "joe-twitter"
@@ -35,4 +7,32 @@ m3o contact update --name="joe" --emails='[
     "label": "facebook",
     "username": "joe-facebook"
   }
-]' --note="this person is very important" 
+]' --note="this person is very important" --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
+  {
+    "label": "home",
+    "number": "010-12345678"
+  },
+  {
+    "label": "work",
+    "number": "010-87654321"
+  }
+]' --emails='[
+  {
+    "address": "home@example.com",
+    "label": "home"
+  },
+  {
+    "address": "work@example.com",
+    "label": "work"
+  }
+]' --links='[
+  {
+    "label": "blog",
+    "url": "https://blog.joe.me"
+  }
+]' --birthday="1995-01-01" --addresses='[
+  {
+    "label": "company address",
+    "location": "123 street address"
+  }
+]' 
