29fcce521564f5f0b4411ddae0b3faaf3d2afc30
$GITHUB_ACTOR <>
2022-03-16 13:29:41 +0000

    Commit from m3o/m3o-cli action

--- examples/image/resize/base64ToBase64ImageWithCropping.sh
+++ examples/image/resize/base64ToBase64ImageWithCropping.sh
@@ -1,4 +1,4 @@
-m3o image resize --cropOptions='{
+m3o image resize --height=100 --cropOptions='{
   "height": 50,
   "width": 50
-}' --base64="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" --width=100 --height=100 
+}' --base64="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" --width=100 
