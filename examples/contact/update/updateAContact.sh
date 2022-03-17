cacb4f5f42c10d7d00aa99d694e953f3a4b98b9f
$GITHUB_ACTOR <>
2022-03-17 16:05:44 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,4 +1,23 @@
-m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
+m3o contact update --links='[
+  {
+    "label": "blog",
+    "url": "https://blog.joe.me"
+  }
+]' --birthday="1995-01-01" --addresses='[
+  {
+    "label": "company address",
+    "location": "123 street address"
+  }
+]' --social_medias='[
+  {
+    "label": "twitter",
+    "username": "joe-twitter"
+  },
+  {
+    "label": "facebook",
+    "username": "joe-facebook"
+  }
+]' --note="this person is very important" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -16,23 +35,4 @@ m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --ph
     "address": "work@example.com",
     "label": "work"
   }
-]' --links='[
-  {
-    "label": "blog",
-    "url": "https://blog.joe.me"
-  }
-]' --note="this person is very important" --birthday="1995-01-01" --addresses='[
-  {
-    "label": "company address",
-    "location": "123 street address"
-  }
-]' --social_medias='[
-  {
-    "label": "twitter",
-    "username": "joe-twitter"
-  },
-  {
-    "label": "facebook",
-    "username": "joe-facebook"
-  }
-]' 
+]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" 
--- examples/evchargers/search/searchWithFiltersFastChargersOnly.sh
+++ examples/evchargers/search/searchWithFiltersFastChargersOnly.sh
@@ -1,6 +1,6 @@
-m3o evchargers search --location='{
+m3o evchargers search --max_results=2 --levels='[
+  "3"
+]' --location='{
   "latitude": 51.53336351319885,
   "longitude": -0.0252
-}' --distance=2000 --max_results=2 --levels='[
-  "3"
-]' 
+}' --distance=2000 
