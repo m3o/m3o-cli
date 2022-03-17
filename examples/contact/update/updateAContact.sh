52eb5d9210a0354437bbca35ec4108db22561e0f
$GITHUB_ACTOR <>
2022-03-17 16:03:40 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,18 +1,4 @@
-m3o contact update --name="joe" --links='[
-  {
-    "label": "blog",
-    "url": "https://blog.joe.me"
-  }
-]' --birthday="1995-01-01" --social_medias='[
-  {
-    "label": "twitter",
-    "username": "joe-twitter"
-  },
-  {
-    "label": "facebook",
-    "username": "joe-facebook"
-  }
-]' --note="this person is very important" --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
+m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -30,9 +16,23 @@ m3o contact update --name="joe" --links='[
     "address": "work@example.com",
     "label": "work"
   }
-]' --addresses='[
+]' --links='[
+  {
+    "label": "blog",
+    "url": "https://blog.joe.me"
+  }
+]' --note="this person is very important" --birthday="1995-01-01" --addresses='[
   {
     "label": "company address",
     "location": "123 street address"
+  }
+]' --social_medias='[
+  {
+    "label": "twitter",
+    "username": "joe-twitter"
+  },
+  {
+    "label": "facebook",
+    "username": "joe-facebook"
   }
 ]' 
--- examples/image/resize/base64ToBase64ImageWithCropping.sh
+++ examples/image/resize/base64ToBase64ImageWithCropping.sh
@@ -1,4 +1,4 @@
-m3o image resize --base64="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" --width=100 --height=100 --cropOptions='{
+m3o image resize --cropOptions='{
   "height": 50,
   "width": 50
-}' 
+}' --base64="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" --width=100 --height=100 
