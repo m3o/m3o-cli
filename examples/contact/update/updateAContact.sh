23312786db107199aca1d2391a55b20b9b44181b
$GITHUB_ACTOR <>
2022-03-20 21:39:33 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,4 +1,4 @@
-m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
+m3o contact update --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -7,7 +7,7 @@ m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
     "label": "work",
     "number": "010-87654321"
   }
-]' --emails='[
+]' --birthday="1995-01-01" --note="this person is very important" --id="42e48a3c-6221-11ec-96d2-acde48001122" --emails='[
   {
     "address": "home@example.com",
     "label": "home"
@@ -21,7 +21,7 @@ m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
     "label": "blog",
     "url": "https://blog.joe.me"
   }
-]' --name="joe" --birthday="1995-01-01" --addresses='[
+]' --addresses='[
   {
     "label": "company address",
     "location": "123 street address"
@@ -35,4 +35,4 @@ m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
     "label": "facebook",
     "username": "joe-facebook"
   }
-]' --note="this person is very important" 
+]' --name="joe" 
---
+++ examples/tunnel/send/sendARequest.sh
@@ -0,0 +1,1 @@
+m3o tunnel send --url="https://google.com" 
