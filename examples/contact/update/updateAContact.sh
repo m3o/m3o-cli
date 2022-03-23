9d5c0c5c67364d3769530fc3f8c2c5c18561d43c
$GITHUB_ACTOR <>
2022-03-23 09:13:11 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,4 +1,4 @@
-m3o contact update --name="joe" --phones='[
+m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -16,7 +16,7 @@ m3o contact update --name="joe" --phones='[
     "address": "work@example.com",
     "label": "work"
   }
-]' --birthday="1995-01-01" --social_medias='[
+]' --social_medias='[
   {
     "label": "twitter",
     "username": "joe-twitter"
@@ -25,12 +25,12 @@ m3o contact update --name="joe" --phones='[
     "label": "facebook",
     "username": "joe-facebook"
   }
-]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --links='[
+]' --links='[
   {
     "label": "blog",
     "url": "https://blog.joe.me"
   }
-]' --addresses='[
+]' --birthday="1995-01-01" --addresses='[
   {
     "label": "company address",
     "location": "123 street address"
--- examples/stock/orderBook/orderBookHistory.sh
+++ examples/stock/orderBook/orderBookHistory.sh
@@ -1,1 +1,1 @@
-m3o stock orderBook --start="2020-10-01T10:00:00Z" --end="2020-10-01T11:00:00Z" --limit=3 --stock="AAPL" --date="2020-10-01" 
+m3o stock orderBook --stock="AAPL" --date="2020-10-01" --start="2020-10-01T10:00:00Z" --end="2020-10-01T11:00:00Z" --limit=3 
