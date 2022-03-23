cd085ff1470b42c0ef40ca3009152bb571858c52
$GITHUB_ACTOR <>
2022-03-23 13:53:07 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,9 +1,4 @@
-m3o contact update --addresses='[
-  {
-    "label": "company address",
-    "location": "123 street address"
-  }
-]' --note="this person is very important" --phones='[
+m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -17,15 +12,6 @@ m3o contact update --addresses='[
     "label": "blog",
     "url": "https://blog.joe.me"
   }
-]' --emails='[
-  {
-    "address": "home@example.com",
-    "label": "home"
-  },
-  {
-    "address": "work@example.com",
-    "label": "work"
-  }
 ]' --birthday="1995-01-01" --social_medias='[
   {
     "label": "twitter",
@@ -35,4 +21,18 @@ m3o contact update --addresses='[
     "label": "facebook",
     "username": "joe-facebook"
   }
-]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" 
+]' --emails='[
+  {
+    "address": "home@example.com",
+    "label": "home"
+  },
+  {
+    "address": "work@example.com",
+    "label": "work"
+  }
+]' --addresses='[
+  {
+    "label": "company address",
+    "location": "123 street address"
+  }
+]' --note="this person is very important" 
---
+++ examples/price/report/reportAPrice.sh
@@ -0,0 +1,1 @@
+m3o price report --name="bitcoin" --symbol="BTC" --comment="Price is not up to date" 
