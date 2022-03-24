e0b4694f4d1ff21d4cb32314addde65b6bcefc8b
$GITHUB_ACTOR <>
2022-03-24 21:36:42 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,9 +1,4 @@
-m3o contact update --birthday="1995-01-01" --addresses='[
-  {
-    "label": "company address",
-    "location": "123 street address"
-  }
-]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
+m3o contact update --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -12,12 +7,7 @@ m3o contact update --birthday="1995-01-01" --addresses='[
     "label": "work",
     "number": "010-87654321"
   }
-]' --links='[
-  {
-    "label": "blog",
-    "url": "https://blog.joe.me"
-  }
-]' --note="this person is very important" --name="joe" --emails='[
+]' --note="this person is very important" --id="42e48a3c-6221-11ec-96d2-acde48001122" --emails='[
   {
     "address": "home@example.com",
     "label": "home"
@@ -25,6 +15,16 @@ m3o contact update --birthday="1995-01-01" --addresses='[
   {
     "address": "work@example.com",
     "label": "work"
+  }
+]' --links='[
+  {
+    "label": "blog",
+    "url": "https://blog.joe.me"
+  }
+]' --birthday="1995-01-01" --addresses='[
+  {
+    "label": "company address",
+    "location": "123 street address"
   }
 ]' --social_medias='[
   {
