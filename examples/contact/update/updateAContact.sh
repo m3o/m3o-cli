0c7b792e12daf73c6eb0c19b41961b5cb843da84
$GITHUB_ACTOR <>
2022-03-31 19:11:56 +0000

    Commit from m3o/m3o-cli action

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,4 +1,4 @@
-m3o contact update --name="joe" --phones='[
+m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -16,17 +16,17 @@ m3o contact update --name="joe" --phones='[
     "address": "work@example.com",
     "label": "work"
   }
-]' --birthday="1995-01-01" --addresses='[
+]' --addresses='[
   {
     "label": "company address",
     "location": "123 street address"
   }
-]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --links='[
+]' --links='[
   {
     "label": "blog",
     "url": "https://blog.joe.me"
   }
-]' --social_medias='[
+]' --birthday="1995-01-01" --social_medias='[
   {
     "label": "twitter",
     "username": "joe-twitter"
