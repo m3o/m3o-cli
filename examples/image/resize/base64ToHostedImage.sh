a95d099c5e3b0361b6a6ff8e0b78045675ae9fef
$GITHUB_ACTOR <>
2022-04-08 09:29:42 +0000

    Commit from m3o/m3o-cli action

--- examples/image/resize/base64ToHostedImage.sh
+++ examples/image/resize/base64ToHostedImage.sh
@@ -1,1 +1,1 @@
-m3o image resize --base64="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" --outputURL='true' --name="cat.png" --width=100 --height=100 
+m3o image resize --height=100 --base64="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" --outputURL='true' --name="cat.png" --width=100 
