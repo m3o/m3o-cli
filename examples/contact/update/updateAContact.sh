9e0d25661061d6651b7839a84b60d06be38791db
m3o-actions <m3o-actions@users.noreply.github.com>
2022-04-27 14:15:40 +0000

    Push to M3O-Cli repo

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,18 +1,4 @@
-m3o contact update --links='[
-  {
-    "label": "blog",
-    "url": "https://blog.joe.me"
-  }
-]' --birthday="1995-01-01" --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
-  {
-    "label": "home",
-    "number": "010-12345678"
-  },
-  {
-    "label": "work",
-    "number": "010-87654321"
-  }
-]' --note="this person is very important" --emails='[
+m3o contact update --emails='[
   {
     "address": "home@example.com",
     "label": "home"
@@ -21,12 +7,12 @@ m3o contact update --links='[
     "address": "work@example.com",
     "label": "work"
   }
-]' --addresses='[
+]' --links='[
   {
-    "label": "company address",
-    "location": "123 street address"
+    "label": "blog",
+    "url": "https://blog.joe.me"
   }
-]' --social_medias='[
+]' --birthday="1995-01-01" --social_medias='[
   {
     "label": "twitter",
     "username": "joe-twitter"
@@ -34,5 +20,19 @@ m3o contact update --links='[
   {
     "label": "facebook",
     "username": "joe-facebook"
+  }
+]' --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
+  {
+    "label": "home",
+    "number": "010-12345678"
+  },
+  {
+    "label": "work",
+    "number": "010-87654321"
+  }
+]' --note="this person is very important" --name="joe" --addresses='[
+  {
+    "label": "company address",
+    "location": "123 street address"
   }
 ]' 
