2cfc342e57dc33918bab9eb13c61d9203241d8ab
m3o-actions <m3o-actions@users.noreply.github.com>
2022-07-15 08:23:01 +0000

    Push to M3O-Cli repo

---
+++ examples/bitcoin/balance/getABitcoinAddressBalance.sh
@@ -0,0 +1,1 @@
+m3o bitcoin balance --address="1MDUoxL1bGvMxhuoDYx6i11ePytECAk9QK" 
--- examples/contact/create/createAContact.sh
+++ examples/contact/create/createAContact.sh
@@ -1,18 +1,4 @@
-m3o contact create --birthday="1995-01-01" --addresses='[
-  {
-    "label": "company address",
-    "location": "123 street address"
-  }
-]' --social_medias='[
-  {
-    "label": "twitter",
-    "username": "joe-twitter"
-  },
-  {
-    "label": "facebook",
-    "username": "joe-facebook"
-  }
-]' --note="this person is very important" --name="joe" --phones='[
+m3o contact create --note="this person is very important" --name="joe" --phones='[
   {
     "label": "home",
     "number": "010-12345678"
@@ -34,5 +20,19 @@ m3o contact create --birthday="1995-01-01" --addresses='[
   {
     "label": "blog",
     "url": "https://blog.joe.me"
+  }
+]' --birthday="1995-01-01" --addresses='[
+  {
+    "label": "company address",
+    "location": "123 street address"
+  }
+]' --social_medias='[
+  {
+    "label": "twitter",
+    "username": "joe-twitter"
+  },
+  {
+    "label": "facebook",
+    "username": "joe-facebook"
   }
 ]' 
