fbdec028f5d2c9871bd90d3713cabc99a782f55b
m3o-actions <m3o-actions@users.noreply.github.com>
2022-07-25 15:36:33 +0000

    Push to M3O-Cli repo

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -1,4 +1,13 @@
-m3o contact update --note="this person is very important" --id="42e48a3c-6221-11ec-96d2-acde48001122" --emails='[
+m3o contact update --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
+  {
+    "label": "home",
+    "number": "010-12345678"
+  },
+  {
+    "label": "work",
+    "number": "010-87654321"
+  }
+]' --note="this person is very important" --emails='[
   {
     "address": "home@example.com",
     "label": "home"
@@ -12,16 +21,7 @@ m3o contact update --note="this person is very important" --id="42e48a3c-6221-11
     "label": "blog",
     "url": "https://blog.joe.me"
   }
-]' --birthday="1995-01-01" --name="joe" --phones='[
-  {
-    "label": "home",
-    "number": "010-12345678"
-  },
-  {
-    "label": "work",
-    "number": "010-87654321"
-  }
-]' --addresses='[
+]' --birthday="1995-01-01" --addresses='[
   {
     "label": "company address",
     "location": "123 street address"
