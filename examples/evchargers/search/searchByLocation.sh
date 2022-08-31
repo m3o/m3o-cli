c9b892bce8b8667e4ce090a22b5a21b018e8e587
m3o-actions <m3o-actions@users.noreply.github.com>
2022-08-31 19:23:45 +0000

    Push to M3O-Cli repo

--- examples/evchargers/search/searchByLocation.sh
+++ examples/evchargers/search/searchByLocation.sh
@@ -1,4 +1,4 @@
-m3o evchargers search --location='{
+m3o evchargers search --distance=2000 --max_results=2 --location='{
   "latitude": 51.53336351319885,
   "longitude": -0.0252
-}' --distance=2000 --max_results=2 
+}' 
